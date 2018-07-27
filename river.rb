a3d12042540a1fd5b1a732b926022a12c57e3bad
Mary Cotter <marebearc@gmail.com>
2018-07-27 20:04:02 +0000

    Done.

--- river.rb
+++ river.rb
@@ -1,1 +1,13 @@
 # river.rb
+class River 
+  attr_accessor :name, :length, :countries, :discharge
+  def initialize(name)
+    @name = name
+  end
+  def flood
+    @discharge *= 1.3 
+  end
+  def dry_up
+    @discharge *= 0.5
+  end
+end
